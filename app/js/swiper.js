97011b4d768a6267c19adffd91ff17989ed8f342
osviatka <svyatka@ukr.net>
2017-01-23 18:36:06 +0200

    test

--- app/js/swiper.js
+++ app/js/swiper.js
@@ -1,3 +1,14 @@
 /**
  * Created by Oleg on 1/22/2017.
  */
+var mySwiper = new Swiper ('.color-container', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+    autoplay: 4000,
+    speed: 1400,
+    autoplayDisableOnInteraction: false,
+
+    // If we need pagination
+    pagination: '.color-pagination'
+});
